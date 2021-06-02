4dba1f33004e22616d95dafd570013ffcb00b84e
AtlasNeiko <atlasneiko@gmail.com>
2021-06-02 10:40:24 -0400

    app basic test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GuessedWords from './component/guessedWords/GuessedWords';
 import Input from './component/input/Input';
 function App() {
   return (
-    <div className='App container'>
+    <div className='App container' data-test='component-app'>
       <Congrats success={true} />
       <GuessedWords
         guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,11 @@ import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
 const setup = () => {
-  shallow(<App />);
+  return shallow(<App />);
 };
 
-test('renders without error', () => {});
+test('renders without error', () => {
+  const wrapper = setup();
+  const app = findByTestAttr(wrapper, 'component-app');
+  expect(app).toHaveLength(1);
+});
